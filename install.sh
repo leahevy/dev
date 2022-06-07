1c4934712a9a2ab6df4fddc31b53ad19d190e759
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-07 23:35:23 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,12 +2,19 @@
 
 set -euo pipefail
 
+# Plain shell scripts
 rm -f ~/bin/*
 mkdir -p ~/bin 
 cp -r scripts/*/* ~/bin/
 
+# My python package
 (cd python; pip install -e .)
 
+# Homebrew
 brew bundle
 
+# Node deps
 npm install -g asciicast2gif
+
+# Gemfile
+bundle install --system
